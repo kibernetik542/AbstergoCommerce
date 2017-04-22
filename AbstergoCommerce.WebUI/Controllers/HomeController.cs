9469ad6a58bccc953f5c505067d693148f2f59bd
kibernetik542 <qwerty5798858@box.az>
2017-04-22 20:50:26 +0400

    Add site layout to project

---
+++ AbstergoCommerce.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System.Web.Mvc;
+
+namespace AbstergoCommerce.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult Cart()
+        {
+            return PartialView();
+        }
+
+        public ActionResult Categories()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ActionResult About()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ActionResult Contact()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public PartialViewResult Slider()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult NewProducts()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult Services()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult Fashion()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult Brands()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ AbstergoCommerce.WebUI/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Mvc;
+
+namespace AbstergoCommerce.WebUI.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Account()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ActionResult Logout()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ActionResult Register()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
